a28fe70296276d8c736754f2f8e167de5a175e51
Sufyan <sm.asd999@gmail.com>
2026-01-15 19:36:08 +0500

    a

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: "/mdp-visualizer/", // <--- ADD THIS LINE (use your repo name)
+  base: "/mdp-visualizer/",
 })
